123c6aa849f4e4d67c61d09156059b8b4a7a1dea
Quoc Trung <0306221087@caothang.edu.vn>
2025-01-06 15:23:19 +0700

    update EditProfile

--- app/src/main/java/com/example/damon/Screen/EditProfile.kt
+++ app/src/main/java/com/example/damon/Screen/EditProfile.kt
@@ -2,24 +2,28 @@ package com.example.damon.Screen
 
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -27,6 +31,10 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,6 +49,9 @@ import androidx.compose.ui.unit.sp
 @Preview
 @OptIn(ExperimentalMaterial3Api::class)
 fun EditProfile() {
+    val scrollState = rememberScrollState()
+    var lsGenders = mutableListOf("Nam", "Nữ", "Khác")
+    var selection by remember {mutableStateOf(lsGenders[0])}
     Scaffold(
         topBar = {
             TopAppBar(
@@ -59,39 +70,15 @@ fun EditProfile() {
                     titleContentColor = Color.Black
                 )
             )
-        },
-        bottomBar = {
-            BottomAppBar(tonalElevation = 0.dp ) {
-                Row (modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Home,
-                            contentDescription = "",
-                            tint = Color.Black
-                        )
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = "",
-                            tint = Color.Black
-                        )
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Person,
-                            contentDescription = "",
-                            tint = Color.Black
-                        )
-                    }
-                }
-            }
         }
     ) {
-        Column(modifier = Modifier.fillMaxSize().padding(it).fillMaxWidth()) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)
+            .fillMaxWidth()
+            .background(color = Color(0xFFF5F5F5))
+            .verticalScroll(rememberScrollState())
+        ) {
             Text(text = "Địa chỉ email",
                 fontSize = 15.sp,
                 fontWeight = FontWeight.Bold,
@@ -241,6 +228,42 @@ fun EditProfile() {
                 shape = RoundedCornerShape(8.dp),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
+            Column(modifier = Modifier.padding(start = 10.dp)){
+                Text(text = "Giới tính",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
+                )
+                lsGenders.forEach{
+                        gender -> Row(
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    RadioButton(
+                        selected = gender == selection,
+                        onClick = {selection = gender},
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = Color.Black,
+                        )
+                    )
+                    Text(text = gender, modifier = Modifier.padding(start = 5.dp, top = 15.dp))
+                }
+                }
+            }
+            Button(onClick = {},
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp)
+                    .height(55.dp)
+                    .align(Alignment.CenterHorizontally),
+                shape = RoundedCornerShape(15.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Lưu",  fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+
         }
     }
 
