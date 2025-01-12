2bcec78376e8b608af8626a9cb350cc15a71ec90
HoangTien <88817335+hoangtien04@users.noreply.github.com>
2025-01-12 20:50:24 +0700

    data class vaf apiservice

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,14 +41,17 @@ android {
 
 dependencies {
 
-    implementation ("androidx.compose.material:material:1.5.1")
-    implementation ("androidx.compose.ui:ui-tooling:1.5.1")
-    implementation ("androidx.compose.ui:ui:1.5.1")
-    implementation ("androidx.activity:activity-compose:1.7.2")
-    implementation ("com.google.accompanist:accompanist-pager:0.24.13-rc")
-    implementation ("com.google.accompanist:accompanist-pager-indicators:0.24.13-rc")
+    implementation("androidx.compose.material:material:1.5.1")
+    implementation("androidx.compose.ui:ui-tooling:1.5.1")
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("com.google.accompanist:accompanist-pager:0.24.13-rc")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.24.13-rc")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.compose.material:material-icons-extended-android:1.7.6")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.google.code.gson:gson:2.11.0")
     implementation(libs.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
