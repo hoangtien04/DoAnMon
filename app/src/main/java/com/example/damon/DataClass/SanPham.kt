1ffe8e6dee17445cce9d317acf8f571c19a32248
Boss <vtmh67@gmail.com>
2025-01-16 11:43:59 +0700

    Hoan Thanh Dang Nhap

---
+++ app/src/main/java/com/example/damon/DataClass/SanPham.kt
@@ -0,0 +1,31 @@
+package com.example.damon.DataClass
+
+data class SanPhamCard(
+    val MaSP:Int,
+    val TenSP:String,
+    val TenLoai:String,
+    val DonGia:Int,
+    val DuongDan:String
+)
+
+data class SanPhamDetail(
+    val MaSP:Int = 0,
+    val TenSP:String = "",
+    val TenLoai:String = "",
+    val DuongDan:String = "",
+    val DonGia:Int = 0,
+    val MoTa:String = "",
+)
+
+data class SanPham(
+    val MaSP:Int,
+    val MaLoai:Int,
+    val TenSP:String,
+    val DonGia:Int,
+    val MoTa:String
+)
+data class sanphamResponse(
+    val protocol: String,
+    val code:String,
+    var url:String
+)
